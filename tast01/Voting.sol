c752153a442819530d9e9fad591dbdaed32703be
zengwangqiu <1036548849@qq.com>
2025-11-03 17:21:01 +0800

    init

---
+++ tast01/Voting.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract Volting {
+    function vote() public payable {}
+    function getVotes() public view {}
+    function resetVotes() public {}
+}
